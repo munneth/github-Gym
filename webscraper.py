d4115b9202788ba8ec526a4d23570c57da7250ae
munneth <munnethgill@gmail.com>
2024-12-17 17:13:53 -0800

    Create webscraper.py

---
+++ webscraper.py
@@ -0,0 +1,58 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+r = requests.get('https://rec.ucdavis.edu/facilityoccupancy')
+
+print(r)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+def getOccupancy():
+    #gathering current arc occupancy
+    s = soup.find('div', class_ = 'occupancy-details')
+    occupancy = s.find_all('strong')
+    occupancy = str(list(occupancy[1]))
+    return occupancy
+
+def getTime():
+    #getting current time
+    currentTime = time.localtime()
+    formattedTime = time.strftime("%H:%M", currentTime)
+    return formattedTime
+def lastUpdateTime():
+    lastUpdate = time()
+    return lastUpdate
+
+def main():
+    timeData = {}
+    printableTimeData = {}
+
+    while getTime() != "00:01":
+        adjTime = getTime()[3:5]
+        #print(adjTime)
+        #print(getTime())
+        if adjTime == "00" or adjTime == "30":
+            timeData[getTime()] = getOccupancy()
+            
+            print(timeData)
+            continue
+        else:
+            continue
+
+    #open and add header to csv
+    f = open("data.csv", "a")
+    header = ["Time", "Occupancy(Max 2500)"]
+    for head in header:
+        f.write(head + ",")
+    f.write("\n")
+
+    #traverse the dictionary and add to csv file
+    for key, value in timeData.items():
+        f.write(key + "," + value + "\n")
+    f.close()
+
+    return timeData
+
+main()
